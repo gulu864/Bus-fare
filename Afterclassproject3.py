0681e9acce3e888e3bc02fcc2038c5fb213238c2
gulu864 <suva.kanta4u@gmail.com>
2025-02-01 17:20:07 +0530

    Add files via upload

--- Afterclassproject3.py
+++ Afterclassproject3.py
@@ -2,8 +2,13 @@ class Vehicle:
 
     def __init__(self):
         print("The Bus is here")
+    
     def Fare(self):
         print("The Bus fare begins!")
+    def Seats(self):
+        print("Bus number 1 has 10 seats.")
+        print("Bus number 2 has 12 seats.")
+        print("Bus number 3 has 16 seats.")
 
 class Bus(Vehicle):
 
